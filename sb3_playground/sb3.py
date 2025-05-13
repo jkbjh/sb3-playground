1ce23b98b985075b07c7133c8093e7e1598ea207
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2025-05-13 09:42:44 +0200

    [wip] update sb3 wrapper.

--- sb3_playground/sb3.py
+++ sb3_playground/sb3.py
@@ -3,66 +3,87 @@ import gym
 import numpy as np
 import jax
 import jax.numpy as jnp
-from infoview import InfoWrapper, InfoElementView
+from infoview import InfoWrapper
+from .utils import split_rng_key
 
 # action_limits = env.mj_model.actuator_ctrlrange
 # env.action_size
 # env.observation_size
 
+
 class Mjx2SB3VecEnv(VecEnv):
-    def __init__(self, env, num_envs, action_space, key):
+    def __init__(self, env, num_envs, rng):
         self.env = env
-        self.num_envs = num_envs
-        self.obs_shape = (env.observation_size,)
-        self._action_space = action_space
+        self._num_envs = num_envs
+        self.rng, self._keys = split_rng_key(rng, (num_envs,))
+
+        # Observation and action space
+        obs_shape = (env.observation_size,)
         self._observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=obs_shape, dtype=np.float32)
-        self._keys = jnp.asarray(jax.random.split(key, num_envs))
-        self._reset = jax.jit(jax.vmap(self.env.reset))
-        self._step = jax.jit(jax.vmap(self.env.step))
-        self._replenish = jax.jit(jax.vmap(self.env.replenish_reset))
-        self._state = self._reset(self._keys)
-        self.reset_infos = [{} for _ in range(num_envs)]
+
+        action_limits = env.mj_model.actuator_ctrlrange
+        self._action_low = np.asarray(action_limits[:, 0])
+        self._action_high = np.asarray(action_limits[:, 1])
+        self._action_space = gym.spaces.Box(low=self._action_low, high=self._action_high, dtype=np.float32)
+
+        # JIT batched functions
+        self._reset_fn = jax.jit(jax.vmap(env.reset))
+        self._step_fn = jax.jit(jax.vmap(env.step))
+        self._replenish_fn = jax.jit(jax.vmap(env.replenish_reset))
+        self._render_fn = jax.jit(jax.vmap(env.render))
+
+        # Initial state
+        self._state = self._reset_fn(self._keys)
+        self.reset_infos = InfoWrapper(self._state.info)
+        self._actions = None
 
     def reset(self):
-        self._keys = jnp.asarray(jax.random.split(jax.random.PRNGKey(np.random.randint(0, 1e6)), self.num_envs))
-        self._state = self._reset(self._keys)
-        obs = np.asarray(self._state.obs)
-        self.reset_infos = [{} for _ in range(self.num_envs)]
-        return obs
+        self.rng, self._keys = split_rng_key(self.rng, (self._num_envs,))
+        self._state = self._reset_fn(self._keys)
+        self.reset_infos = InfoWrapper(self._state.info)
+        return np.asarray(self._state.obs)
 
     def step_async(self, actions):
-        self._actions = jnp.array(actions)
+        actions = np.asarray(actions)
+        clipped_actions = np.clip(actions, self._action_low, self._action_high)
+        self._actions = jnp.asarray(clipped_actions)
+        self._next_state = self._step_fn(self._state, self._actions)
 
     def step_wait(self):
-        self._state = self._step(self._state, self._actions)
+        self._state = self._next_state
         obs = np.asarray(self._state.obs)
         rewards = np.asarray(self._state.reward)
         dones = np.asarray(self._state.done).astype(bool)
-        infos = []
 
-        for i in range(self.num_envs):
-            info = {}
-            if dones[i]:
-                info["TimeLimit.truncated"] = bool(self._state.info["truncation"][i])
-                info["terminal_observation"] = np.asarray(self._state.info["last_obs"][i])
-            infos.append(info)
+        state_info = self._state.info
 
-        # Replenish after terminal envs
+        # Add SB3 aliases directly into info
+        terminations = np.asarray(state_info["termination"]).astype(bool)
+        truncations = np.asarray(state_info["truncation"]).astype(bool)
+        time_limit_flags = truncations & ~terminations
+
+        state_info["TimeLimit.truncated"] = time_limit_flags
+        state_info["terminal_observation"] = state_info["last_obs"]
+
+        infos = InfoWrapper(state_info)
+
+        # Auto-reset if needed
         if np.any(dones):
-            replenish_keys = jnp.asarray(jax.random.split(jax.random.PRNGKey(np.random.randint(0, 1e6)), self.num_envs))
-            self._state = self._replenish(replenish_keys, self._state)
+            self.rng, replenish_keys = split_rng_key(self.rng, (self._num_envs,))
+            self._state = self._replenish_fn(replenish_keys, self._state)
 
         return obs, rewards, dones, infos
 
-    def close(self):
-        pass
-
     def render(self, mode="rgb_array"):
-        raise NotImplementedError("Rendering is not implemented.")
+        return self._render_fn(self._state)
 
     def seed(self, seed=None):
         if seed is not None:
-            self._keys = jnp.asarray(jax.random.split(jax.random.PRNGKey(seed), self.num_envs))
+            self.rng = jax.random.PRNGKey(seed)
+            self.rng, self._keys = split_rng_key(self.rng, (self._num_envs,))
+
+    def close(self):
+        pass
 
     @property
     def num_envs(self):
